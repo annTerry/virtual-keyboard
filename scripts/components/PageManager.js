61faaa960a5321cf1190b2c332e267c7afc08541
Anna <89348633+annTerry@users.noreply.github.com>
2023-05-01 16:06:15 +0300

    feat: add key animation

--- scripts/components/PageManager.js
+++ scripts/components/PageManager.js
@@ -1,12 +1,16 @@
 import TextArea from './TextArea.js';
 import Keyboard from './Keyboard.js';
+import { createElement } from '../common/utils.js';
 
 export default class PageManager {
   textarea = new TextArea();
 
+  header = createElement('h1','header','Virtual Keyboard');
+
   keyboard = new Keyboard(this.textarea);
 
   constructor() {
+    document.body.append(this.header);
     window.addEventListener('keydown', (event) => {
       event.preventDefault();
       event.stopImmediatePropagation();
